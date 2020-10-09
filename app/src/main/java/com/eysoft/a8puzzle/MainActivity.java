19f016ca4e05875a5b876fe2ea3857cf715913f0
retrx0 <abdulrahmaniliyasu86@gmail.com>
2020-10-09 20:33:01 +0200

    Toolbar

--- app/src/main/java/com/eysoft/a8puzzle/MainActivity.java
+++ app/src/main/java/com/eysoft/a8puzzle/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.gridlayout.widget.GridLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,16 +32,17 @@ public class MainActivity extends AppCompatActivity {
         for (int i =1; i<9; i++){
             Button btn = new Button(this);
             btn.setText(""+i);
-            btn.setHeight(20);
+            btn.setHeight(30);
             btn.setWidth(20);
             puzzleGrid.addView(btn);
         }
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-//        menuInflater.inflate(R.);
         menuInflater.inflate(R.menu.main_menu, menu);
         return true;
     }
@@ -47,4 +50,19 @@ public class MainActivity extends AppCompatActivity {
     public void solveButtonOnClick(View view) {
 
     }
+
+    public void menuItemOnClick(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.settingsMenu:
+                Intent intent = new Intent(this,SettingsActivity.class);
+                MainActivity.this.startActivity(intent);
+                break;
+            case R.id.aboutMenu:
+                Intent intent_about = new Intent(this,AboutActivity.class);
+                MainActivity.this.startActivity(intent_about);
+                break;
+        }
+
+        }
 }
---
+++ app/src/main/java/com/eysoft/a8puzzle/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.eysoft.a8puzzle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
