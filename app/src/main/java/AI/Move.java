62f6735a2d319621af0ac889499681bc5e399684
retrx0 <abdulrahmaniliyasu86@gmail.com>
2020-10-09 22:20:20 +0200

    AI

---
+++ app/src/main/java/AI/Move.java
@@ -0,0 +1,40 @@
+package AI;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Move {
+
+    public enum Action {LEFT, RIGHT, UP, DOWN};
+
+    public final int fromIndex;
+    public final Action action;
+    private final Board fromBoard;
+
+    public Move(Board frmBoard, Action action) {
+        switch (action){
+            case UP: fromIndex = frmBoard.blankIndex-frmBoard.size; break;
+            case DOWN: fromIndex = frmBoard.blankIndex+frmBoard.size; break;
+            case LEFT: fromIndex = frmBoard.blankIndex+1; break;
+            case RIGHT: fromIndex = frmBoard.blankIndex-1;break;
+            default: fromIndex = -1;
+        }
+        this.action = action;
+        this.fromBoard = frmBoard;
+    }
+
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        Move other = (Move) obj;
+        return fromIndex == other.fromIndex && action == other.action;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        int row  = fromBoard.getRow((fromIndex));
+        int col = fromBoard.getCol(fromIndex);
+        return "(" +col+ ", "+ row+") "+ action;
+    }
+}
